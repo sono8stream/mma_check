36098350fe52ec497978341aa342f85d4d57c16e
SONO <sono.stream8580@gmail.com>
2024-07-28 20:31:20 +0900

    Port deconv process

--- hello.cpp
+++ hello.cpp
@@ -385,117 +385,75 @@ void mmaDeconv(){
        3,
     };
 
-    int numInChannels = 1;
-    int numOutChannels = 1;
-    int numGroupsPerKernel = 1;
-
     MMALIB_bufParams2D_t kernelBuffer;
-    int kDim = 3 * 3 * numInChannels;
-    kernelBuffer.dim_x = kDim;
-    kernelBuffer.dim_y = numOutChannels * numGroupsPerKernel;// numOfOutputChKerBuf * numGroupsPerKernel
-    kernelBuffer.stride_y = kDim;// pitchA
-    kernelBuffer.data_type = MMALIB_INT8;
-
-    int inChOffset = 772;
+    kernelBuffer.dim_x = param.kDim;
+    kernelBuffer.dim_y = param.numOfOutputChKerBuf * param.numGroups;
+    kernelBuffer.stride_y = param.pitchA;
+    kernelBuffer.data_type = param.dataTypeA;
 
     MMALIB_bufParams2D_t srcBuffer;
-    srcBuffer.dim_x = inChOffset;// inChOffset
-    srcBuffer.dim_y = numInChannels * numGroupsPerKernel;// numInChannels*numGroupsPerKernel
-    srcBuffer.stride_y = inChOffset;// inChOffset
-    srcBuffer.data_type = MMALIB_INT8;
+    srcBuffer.dim_x = param.inChOffset;// inChOffset
+    srcBuffer.dim_y = param.numInChannels * param.numGroups;// numInChannels*numGroupsPerKernel
+    srcBuffer.stride_y = param.inChOffset;// inChOffset
+    srcBuffer.data_type = param.dataTypeB;
 
-    int kernelWidth = 3;
-    int kernelHeight = 3;
-    int numBiasVals = kDim - kernelWidth*kernelHeight*numInChannels;// 今回はゼロ。
-    int numBytes = 1;
+    MMALIB_bufParams2D_t dstBuffer;
+    dstBuffer.dim_x = param.pitchC;
+    dstBuffer.dim_y = param.numOutChannels * param.numGroups;// numOutChannels
+    dstBuffer.stride_y = param.pitchC;
+    dstBuffer.data_type = param.dataTypeC;
 
-    MMALIB_bufParams2D_t biasBuffer;
-    biasBuffer.dim_x = numBiasVals;// numBiasVals
-    biasBuffer.dim_y = numOutChannels;// numOutChannels
-    biasBuffer.stride_y = numBiasVals * numBytes;// 今回はゼロ。
-    biasBuffer.data_type = MMALIB_INT8;// カーネルと同じデータ型。
-
-    int pitchC = 576;// validColsOut(256) * 2 + align
-
-    MMALIB_bufParams3D_t dstBuffer;
-    dstBuffer.dim_x = pitchC / 1;// pitchC/numBytes
-    dstBuffer.dim_y = numOutChannels;// numOutChannels
-    dstBuffer.stride_y = pitchC;
-    dstBuffer.dim_z = numGroupsPerKernel;// numGroupsPerKernel
-    dstBuffer.stride_z = numOutChannels * pitchC;// numOutChannels*pitchC
-    dstBuffer.data_type = MMALIB_UINT8;
-
-    int dilationWidth = 1;
-    int dilationHeight = 1;
-    int strideWidth = 1;
-    int strideHeight = 1;
-    int validColsIn = 772;
-    int subMChannels = 1;
-    int inWidth = 256;
-    int pad = 1;
-    int maxHeight = 256;
-
-    MMALIB_CNN_convolve_row_ixX_ixX_oxX_InitArgs initArgs;
-    initArgs.funcStyle = MMALIB_FUNCTION_NATC;
-    initArgs.No = numOutChannels;
-    initArgs.inChOffset = inChOffset;
-    initArgs.validColsIn = validColsIn;
-    initArgs.validColsPerRowIn = 0;
-    initArgs.validRowsIn = 0;
-    initArgs.inputPitchPerRow = 0;
-    initArgs.outputPitchPerRow = 0;
-    initArgs.inWidth = inWidth;
-    initArgs.pad = 1;
-    initArgs.maxHeight = maxHeight;
-    initArgs.subMChannels = subMChannels;
-    initArgs.numGroupsPerKernel = numGroupsPerKernel;
-    initArgs.shift = 0;
-    initArgs.Fr = kernelWidth;
-    initArgs.Fc = kernelHeight;
-    initArgs.strideX = strideWidth;
-    initArgs.strideY = strideHeight;
-    initArgs.dilationX = dilationWidth;
-    initArgs.dilationY = dilationHeight;
-    initArgs.bias = 0;
-    initArgs.activationType = MMALIB_RELU;
-    initArgs.mode = MMALIB_LINEAR;
-    initArgs.weightReorderFlag = 0;
-    initArgs.numBiasVals = numBiasVals;
+    MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_InitArgs kerInitArgs;
+    kerInitArgs.Fc                = param.kernelWidth;
+    kerInitArgs.Fr                = param.kernelHeight;
+    kerInitArgs.dilationX         = param.dilationWidth;
+    kerInitArgs.dilationY         = param.dilationHeight;
+    kerInitArgs.strideX           = param.strideWidth;
+    kerInitArgs.strideY           = param.strideHeight;
+    kerInitArgs.validColsIn       = param.validColsIn;
+    kerInitArgs.subMChannels      = param.subMChannels;
+    kerInitArgs.inWidth           = param.inWidth;
+    kerInitArgs.pad               = param.pad;
+    kerInitArgs.maxHeight         = param.maxHeight;
+    kerInitArgs.inChOffset        = param.inChOffset;
+    kerInitArgs.shift             = param.qShift;
+    kerInitArgs.No                = param.numOutChannels;
+    kerInitArgs.numGroups         = param.numGroups;
+    kerInitArgs.bias              = param.biasB;
+    kerInitArgs.activationType    = param.activationType;
+    kerInitArgs.mode              = param.mode;
+    kerInitArgs.validColsPerRowIn = param.validColsPerRowIn;
+    kerInitArgs.validRowsIn       = param.validRowsIn;
+    kerInitArgs.outputPitchPerRow = param.outputPitchPerRow;
+    kerInitArgs.inputPitchPerRow  = param.inputPitchPerRow;
 
 
 
-    int32_t handleSize = MMALIB_CNN_convolve_row_ixX_ixX_oxX_getHandleSize(&initArgs);
+    int32_t handleSize = MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_getHandleSize(&kerInitArgs);
     MMALIB_kernelHandle kernelHandle = malloc(handleSize);
 
     // Check that the parameters will generate a valid handle
-    MMALIB_STATUS initCheck = MMALIB_CNN_convolve_row_ixX_ixX_oxX_init_checkParams(kernelHandle,
+    MMALIB_STATUS initCheck = MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_init_checkParams(kernelHandle,
         &kernelBuffer,
         &srcBuffer,
         &dstBuffer,
-        &initArgs);
+        &kerInitArgs);
 
     printf("Init check = %d.\n", initCheck);
 
     // Generate the handle
-    MMALIB_STATUS initStatus = MMALIB_CNN_convolve_row_ixX_ixX_oxX_init(kernelHandle,
+    MMALIB_STATUS initStatus = MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_init(kernelHandle,
                                                                         &kernelBuffer,
                                                                         &srcBuffer,
                                                                         &dstBuffer,
-                                                                        &initArgs);
+                                                                        &kerInitArgs);
     printf("Init status = %d.\n", initStatus);
 
-    int validColsInLast = 772;
-    int validColsPerRowInLast = 0;
-    int validRowsInLast = 0;
+    MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_ExecInArgs kerExecInArgs;
+    kerExecInArgs.subMChannels = param.subMChannels;
+    kerExecInArgs.validColsIn       = param.validColsIn;
 
-    MMALIB_CNN_convolve_row_ixX_ixX_oxX_ExecInArgs kerExecInArgs;
-    kerExecInArgs.subMChannels = subMChannels;
-    kerExecInArgs.validColsIn       = validColsInLast;
-    kerExecInArgs.validColsPerRowIn = validColsPerRowInLast;
-    kerExecInArgs.validRowsIn       = validRowsInLast;
-    kerExecInArgs.pad               = pad;
-
-    MMALIB_CNN_convolve_row_ixX_ixX_oxX_ExecOutArgs kerExecOutArgs;
+    MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_ExecOutArgs kerExecOutArgs;
 
     int8_t* kernel = (int8_t*)malloc(9);
     for(int i=0;i<9;i++){
@@ -522,17 +480,17 @@ void mmaDeconv(){
         dst[i]=0;
     }
 
-    MMALIB_STATUS execCheck = MMALIB_CNN_convolve_row_ixX_ixX_oxX_exec_checkParams(kernelHandle,
-                                                                                   kernel,
-                                                                                   src,
-                                                                                   dst,
+    MMALIB_STATUS execCheck = MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_exec_checkParams(kernelHandle,
+                                                                                   param.staticKernel8Bit,
+                                                                                   param.staticIn8Bit,
+                                                                                   param.staticOutMMALIB8Bit,
                                                                             &kerExecInArgs);
     printf("Exec check = %d.\n", execCheck);
 
-    MMALIB_STATUS execStatus = MMALIB_CNN_convolve_row_ixX_ixX_oxX_exec(kernelHandle,
-                                                                        kernel,
-                                                                        src,
-                                                                        dst,
+    MMALIB_STATUS execStatus = MMALIB_CNN_deconvolve_row_ixX_ixX_oxX_exec(kernelHandle,
+                                                                        param.staticKernel8Bit,
+                                                                        param.staticIn8Bit,
+                                                                        param.staticOutMMALIB8Bit,
                                                                         &kerExecInArgs,
                                                                         &kerExecOutArgs);
     printf("Exec status = %d.\n", execStatus);
@@ -546,7 +504,8 @@ int main(void)
     printf("Hello World!\n");
 
     //mmaTest();
-    mmaTest2();
+    //mmaTest2();
+    mmaDeconv();
 
     //seTestTranspose();
 
